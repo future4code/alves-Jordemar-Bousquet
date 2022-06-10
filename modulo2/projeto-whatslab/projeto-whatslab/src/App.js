aaca1638e54e94f6c993a98466d95a1bbff324f9
Jord Bousquet <jordbousquet@yahoo.com.br>
2022-06-10 15:52:53 -0300

    Projeto WhatsLab

---
+++ modulo2/projeto-whatslab/projeto-whatslab/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <div className ='borda'>
+     
+      <input className ="usuario" placeholder = "Usuário" type={'text'}></input>
+      <input className= "mensagem" placeholder = "Mensagem" type={'text'}></input>
+      <button>Enviar</button>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;

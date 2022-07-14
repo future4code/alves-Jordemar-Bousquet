50f1756235bf952477b50237a6f4009078208480
Jord Bousquet <jordbousquet@yahoo.com.br>
2022-07-14 00:59:38 -0300

    Estilizando HomePage

--- modulo3/projeto-labex/projeto-labex/src/components/pages/HomePage/HomePage.js
+++ modulo3/projeto-labex/projeto-labex/src/components/pages/HomePage/HomePage.js
@@ -1,6 +1,13 @@
 import React from 'react'
+import { ThemeProvider } from '@material-ui/core/styles';
 import { useNavigate } from 'react-router-dom'
 import { goToListTrip, goToLogin } from '../../Routes/coordinator'
+import Button from '@material-ui/core/Button';
+import { theme } from '../../../Theme';
+import Grid from '@material-ui/core/Grid';
+import { GridContainer, ContainerLoginButton , BackgroudImage } from './StyledHomePage';
+import Typography from '@material-ui/core/Typography';
+
 
 const HomePage = () => {
 
@@ -8,14 +15,30 @@ const navigate = useNavigate()
 
 
   return (
-    <div>
+    <BackgroudImage>
+    <ThemeProvider theme={theme}>
+      
+      
+      <ContainerLoginButton>
+      <Button color="primary" onClick={() => goToLogin(navigate)}>Login</Button>
+      </ContainerLoginButton>
+      <GridContainer>
+       <Grid
+        container
+        direction="column"
+        justifyContent="space-between"
+        alignItems="center"
+        >
         
-        <h1>LabeX</h1>
-        <h3>Encontre as Melhores Viagens Espaciais</h3>
-        <button onClick={() => goToListTrip(navigate)}>Ver Viagens</button>
-        <button onClick={() => goToLogin(navigate)}>Login</button>
+        <Typography variant="h2" gutterBottom >LabeX</Typography>
+        <Typography variant="h4" gutterBottom>Encontre as Melhores Viagens Espaciais !</Typography>
+        <br/>
+        <Button variant="outlined" color="primary" onClick={() => goToListTrip(navigate)}>Ver Viagens</Button>
+        </Grid>
+        </GridContainer>
         
-    </div>
+    </ThemeProvider>
+    </BackgroudImage>
   )
 }
 
---
+++ modulo3/projeto-labex/projeto-labex/src/components/pages/HomePage/StyledHomePage.js
@@ -0,0 +1,16 @@
+import styled from "styled-components"
+
+export const GridContainer = styled.div `
+        padding:25vh;
+
+`
+export const ContainerLoginButton = styled.div`
+        display:flex;
+        flex-direction:row-reverse;
+        padding: 15px;
+`
+
+export const BackgroudImage = styled.div `
+        
+
+`

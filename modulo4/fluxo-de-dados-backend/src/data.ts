7f9a483c9d653e98f687000153069fb3d588df93
JordBousquet <jordbousuqet@yahoo.com.br>
2022-08-17 23:49:57 -0300

    Fnalizando

--- modulo4/fluxo-de-dados-backend/src/data.ts
+++ modulo4/fluxo-de-dados-backend/src/data.ts
@@ -1,7 +1,7 @@
 //ex2//
 
 type Produtos = {
-    id: number,
+    id: string,
     name: string,
     price: number
 
@@ -9,17 +9,17 @@ type Produtos = {
 
 export const games: Produtos[] = [
     {
-        id:1660753960864,
+        id:"1660753960864",
         name: 'God of War Ragnarok',
         price: 350.00
     },
     {
-        id: 1660753960865,
+        id: "1660753960865",
         name: 'Horizon Forbbiden West',
         price: 250.00
     },
     {
-        id: 1660753960866,
+        id: "1660753960866",
         name: 'The Last of Us Part II',
         price: 200.00
     },
--- modulo4/fluxo-de-dados-backend/src/index.ts
+++ modulo4/fluxo-de-dados-backend/src/index.ts
@@ -14,15 +14,33 @@ app.get('/test',(req:Request, res:Response) =>{
 //ex3 //
 app.post('/AddGame',(req:Request,res:Response) =>{
     const {name,price} = req.body
-    console.log(name,price)
     games.push({
-        id:Date.now(),
+        id:Date.now().toString(),
         name:name,
         price:price
     })
-    console.log(games)
+    res.status(400).send(games)
 
 })
+//ex4 // 
+app.get('/produtos',(req:Request,res:Response) =>{
+    res.send(games)
+})
+//ex 5 // 
+app.put('/produtos/:id',(req:Request, res:Response) =>{
+        const id = req.params.id
+        const newPrice = req.body.price
+        console.log(newPrice)
+       const gamesId = games.findIndex(game => game.id === id)
+       console.log(gamesId)
+       if(gamesId < 0){
+        res.status(404).send('Usuário não encontrado')
+       } else {
+        games[gamesId].price == newPrice
+       }
+       res.status(200).send({"Preço modificado com suceso": games})
+})
+
 
 app.listen(3003, () => {
     console.log("Server is running in http://localhost:3003")

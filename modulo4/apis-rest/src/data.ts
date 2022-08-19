fa9bfc7bf0954afff35cb28ca797d651ddb6d2fa
JordBousquet <jordbousuqet@yahoo.com.br>
2022-08-19 00:05:25 -0300

    Finalizando Exercícios

--- modulo4/apis-rest/src/data.ts
+++ modulo4/apis-rest/src/data.ts
@@ -1,10 +1,17 @@
-type Users ={
+enum UserType {
+    ADMIN = "ADMIN",
+    NORMAL = "NORMAL"
+}
+
+
+export type Users ={
     id:number,
     name:string,
     email:string,
-    type:string,
+    type:UserType,
     age:number
 }
+
 
 
 export let users:Users[] = [
@@ -12,42 +19,42 @@ export let users:Users[] = [
         id: 1,
         name: "Alice",
         email: "alice@email.com",
-        type: "ADMIN",
+        type: UserType.ADMIN,
         age: 12
     },
     {
         id: 2,
         name: "Bob",
         email: "bob@email.com",
-        type: "NORMAL",
+        type: UserType.NORMAL,
         age: 36
     },
     {
         id: 3,
         name: "Coragem",
         email: "coragem@email.com",
-        type: "NORMAL",
+        type: UserType.NORMAL,
         age: 21
     },
     {
         id: 4,
         name: "Dory",
         email: "dory@email.com",
-        type: "NORMAL",
+        type: UserType.NORMAL,
         age: 17
     },
     {
         id: 5,
         name: "Elsa",
         email: "elsa@email.com",
-        type: "ADMIN",
+        type: UserType.ADMIN,
         age: 17
     },
     {
         id: 6,
         name: "Fred",
         email: "fred@email.com",
-        type: "ADMIN",
+        type: UserType.ADMIN,
         age: 60
     }
 ]

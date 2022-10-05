d9b01bea0e37e8a3eee09b13fac75caad41d6ea9
JordBousquet <jordbousuqet@yahoo.com.br>
2022-10-05 15:30:44 -0300

    Fazendo a requisição da API de filmes

--- modulo6/rodada-cases-1/listagem-de-filmes/src/components/MainPage/MainPage.jsx
+++ modulo6/rodada-cases-1/listagem-de-filmes/src/components/MainPage/MainPage.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React , {useEffect, useState} from 'react'
 import { useNavigate } from 'react-router-dom'
 import {goToDetailpage} from '../Routes/Coordinator'
 import {Container, MainTitle} from './MainStyled'
+import { BASE_URL, IMAGE_URL } from '../constants/urls'
+import { APIKEY } from '../constants/key'
+
+import axios from 'axios'
 
 const MainPage = () => {
-
 const navigate = useNavigate()
+const [MovieList, SetMovieList] = useState("")
+
+
+useEffect(() =>{
+  getMoviePopular();
+},[])
+
+
+
+const getMoviePopular = () =>{
+  const url = `${BASE_URL}/movie/popular${APIKEY}`
+
+  axios.get(url)
+  .then((resp)=>{
+    SetMovieList(resp.data.results)
+  })
+  .catch((err) =>{
+    console.log(err)
+  })
+
+}
+
+console.log(MovieList)
+
+const MovieTitle = MovieList && MovieList.map((movie) =>{
+
+  const poster = movie.poster_path
+
+    return <div>
+      <h4>{movie.title}</h4>
+      <img src = {`${IMAGE_URL}${poster}`} style = {{width:"250px"}}/>
+      <p>{movie.release_date}</p>
+    </div>
+})
 
 
   return (
@@ -15,7 +52,7 @@ const navigate = useNavigate()
     </Container>
     <button onClick={() => goToDetailpage(navigate)}>Detalhes</button>
     <h1>
-      fimles
+      {MovieTitle}
     </h1>
     </div>
     

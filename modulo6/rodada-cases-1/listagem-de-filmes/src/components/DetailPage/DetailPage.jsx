57acc28794625b82dc0676f802855d6372169f47
JordBousquet <jordbousuqet@yahoo.com.br>
2022-10-07 23:52:07 -0300

    Colocando Elenco na tela de detalhes

--- modulo6/rodada-cases-1/listagem-de-filmes/src/components/DetailPage/DetailPage.jsx
+++ modulo6/rodada-cases-1/listagem-de-filmes/src/components/DetailPage/DetailPage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { goToMainPage } from '../Routes/Coordinator'
-import { Container } from './DetailStyled'
+import { Container, Title, GenderList, Sinopse, ImgPoster,CastContainer, CastImage, CastTitle} from './DetailStyled'
 import { BASE_URL, IMAGE_URL } from '../constants/urls'
 import { APIKEY } from '../constants/key'
 import axios from 'axios'
@@ -12,10 +12,12 @@ const DetailPage = () => {
   const navigate = useNavigate()
   const params = useParams()
   const [Movie, setMovie] = useState('')
+  const [Cast, setCast] = useState('')
 
 
   useEffect(() => {
     getMovieById();
+    getCastbyd();
   }, [])
 
 
@@ -32,26 +34,58 @@ const DetailPage = () => {
 
   }
 
-  console.log(Movie)
+const getCastbyd = () => {
+  const url = `${BASE_URL}/movie/${params.id}/credits${APIKEY}&language=pt-BR`
+
+  axios.get(url)
+  .then((resp) => {
+    setCast(resp.data.cast)
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+}
+
+
+console.log(Cast)
+
 
   const GenresMovie = Movie.genres && Movie.genres.map((gen) => {
-    return <ul>
-      <li>{gen.name}</li>
-    </ul>
+    return <p>
+      <>{gen.name},</>
+    </p>
+  })
+
+  const CanstInfo = Cast && Cast.map((info)=>{
+    return <div>
+      <CastImage src={`${IMAGE_URL}${info.profile_path}`} style={{ width: "150px" }} />
+    <h4>{info.name}</h4>
+    <p>{info.character}</p>
+    </div>
   })
 
 
   return (
     <div>
       <Container>
-        <img src={`${IMAGE_URL}${Movie.poster_path}`} style={{ width: "380px" }} />
-        <h2>{Movie.title}</h2>
+        <Title>
+        <h1>{Movie.title}</h1>
+        </Title>
+        <GenderList>
         {GenresMovie}
-        <strong>Sinopse</strong>
-        <p>{Movie.overview}</p>
+        </GenderList>
+        
+        <Sinopse>
+        <h3>Sinopse</h3>
+          {Movie.overview}
+          </Sinopse>
       </Container>
+      <ImgPoster src={`${IMAGE_URL}${Movie.poster_path}`} style={{ width: "418px" }} />
+      <CastTitle>Elenco Original</CastTitle>
+      <CastContainer>
+      {CanstInfo}
+      </CastContainer>
       <button onClick={() => goToMainPage(navigate)}>Pagina Principal</button>
-
     </div>
   )
 }
--- modulo6/rodada-cases-1/listagem-de-filmes/src/components/DetailPage/DetailStyled.jsx
+++ modulo6/rodada-cases-1/listagem-de-filmes/src/components/DetailPage/DetailStyled.jsx
@@ -4,8 +4,51 @@ export const Container = styled.div`
 display: flex;
 flex-direction: column;
 background-color:#2d0c5d;
-padding:40px;
+padding:50px 0px 150px 525px;
+
 color:white;
 
+`
 
+
+export const Title = styled.div`
+    display: flex;
+    margin-top: 13px;
+   
+`
+
+export const GenderList = styled.div`
+    display:flex;
+    flex-direction: row;
+    margin-bottom: 120px;
+`
+
+export const Sinopse = styled.p`
+    text-align:justify;
+    margin:0px 250px 0px 0px ;
+
+`
+
+export const ImgPoster = styled.img`
+    position:absolute;
+    top:133px;
+    margin-left:6%;
+
+
+`
+
+export const CastImage = styled.img`
+    margin-top: 55px;
+    padding: 8px;
+   
+`
+export const CastContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    margin-left: 55px;
+`
+
+export const CastTitle = styled.h2`
+    padding-left: 85px;
+    padding-top:  55px;
 `
